726cbfbed553d5cfedfe24ea33121a4817abc59f
Max <61809127+maxwellpark@users.noreply.github.com>
2022-10-01 20:46:51 +0100

    Timer trigger and UI (#3)
    * Track time and trigger event
    
    * Counting up timer UI
    
    * Timer stopped flag and rest

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,48 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerManager : StaticMonoBehaviour<TimerManager>
+{
+    public TimeSpan CurrentTime { get; private set; }
+    private float _secondsPast;
+    private float _nextEventTimeInSeconds;
+
+    public bool timerStopped;
+
+    [SerializeField]
+    private float _secondsBetweenEvents = 10;
+
+    // Fires every n seconds
+    public static event UnityAction onTimerTriggered;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTimer();
+    }
+
+    public void ResetTimer()
+    {
+        _secondsPast = 0;
+        _nextEventTimeInSeconds = _secondsBetweenEvents;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerStopped)
+            return;
+
+        _secondsPast += Time.deltaTime;
+        CurrentTime = TimeSpan.FromSeconds(_secondsPast);
+
+        if (_secondsPast >= _nextEventTimeInSeconds)
+        {
+            // Trigger event
+            Debug.Log($"Timer triggered after {_nextEventTimeInSeconds} seconds.");
+            _nextEventTimeInSeconds += _secondsBetweenEvents;
+            onTimerTriggered?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,33 @@
+using System.Globalization;
+using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _timerText;
+    private TimerManager _timerManager;
+
+    private void Start()
+    {
+        _timerManager = TimerManager.GetInstance();
+    }
+
+    private void Update()
+    {
+        if (_timerManager.timerStopped)
+            return;
+
+        _timerText.text = FormatText();
+    }
+
+    private string FormatText()
+    {
+        var text = string.Format(CultureInfo.CurrentCulture, "{0}:{1}:{2}",
+            _timerManager.CurrentTime.Minutes,
+            _timerManager.CurrentTime.Seconds,
+            _timerManager.CurrentTime.Milliseconds);
+
+        return text;
+    }
+}
