179b6a77e9a3c96247e50704b34ce282b91a1480
Max <61809127+maxwellpark@users.noreply.github.com>
2022-10-01 13:44:13 +0100

    Add more boilerplate

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MusicState
+{
+    Battle, BattleEnd, Menu, None
+}
+
+public class MusicManager : StaticMonoBehaviour<MusicManager>
+{
+    [SerializeField]
+    private AudioSource battleSrc;
+
+    [SerializeField]
+    private AudioSource battleEndSrc;
+
+    [SerializeField]
+    private AudioSource menuSrc;
+
+    private AudioSource _currentSrc;
+
+    private Dictionary<MusicState, AudioSource> _srcMap;
+    private Dictionary<string, MusicState> _sceneMap;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        menuSrc.enabled = true;
+        battleSrc.enabled = true;
+        battleEndSrc.enabled = true;
+
+        _srcMap = new Dictionary<MusicState, AudioSource>
+        {
+            { MusicState.Battle, battleSrc },
+            { MusicState.BattleEnd, battleEndSrc },
+            { MusicState.Menu, menuSrc },
+            { MusicState.None, null }
+        };
+
+        _sceneMap = new Dictionary<string, MusicState>
+        {
+            { "BattleScene", MusicState.Battle },
+            { "BattleEndScene", MusicState.BattleEnd },
+            { "BattleMenuScene", MusicState.Menu },
+            { "TitleScreenScene", MusicState.Menu },
+            { "CharacterSelectScene", MusicState.Menu },
+            { "CreditsScene", MusicState.Menu },
+            { "TutorialScene", MusicState.Menu }
+        };
+        _currentSrc = null;
+    }
+
+    public void PlayMusic(MusicState state)
+    {
+        var src = GetSrcByState(state);
+
+        if (src == null || src.isPlaying)
+            return;
+
+        ResetSrcs();
+        src.Play();
+        _currentSrc = src;
+    }
+
+    private void ResetSrcs()
+    {
+        battleSrc.Stop();
+        battleEndSrc.Stop();
+        menuSrc.Stop();
+    }
+
+    private AudioSource GetSrcByState(MusicState state)
+    {
+        Debug.Log("Getting audio src by music state: " + state);
+
+        if (_srcMap.ContainsKey(state))
+            return _srcMap[state];
+
+        Debug.LogWarning("No item exists in src map with state key: " + state);
+        return null;
+    }
+
+    public MusicState GetStateBySrc(AudioSource src)
+    {
+        foreach (var item in _srcMap)
+        {
+            if (item.Value == src)
+                return item.Key;
+        }
+        Debug.LogWarning("No key found in src map for audio src: " + src);
+        return MusicState.None;
+    }
+
+    public MusicState GetStateByScene(string sceneName)
+    {
+        Debug.Log("Getting music state for scene: " + sceneName);
+
+        if (_sceneMap.ContainsKey(sceneName))
+            return _sceneMap[sceneName];
+
+        Debug.LogWarning("No key found in scene map with key: " + sceneName);
+        return MusicState.None;
+    }
+
+    public MusicState GetCurrentState()
+    {
+        var state = GetStateBySrc(_currentSrc);
+        return state;
+    }
+}
---
+++ Assets/Scripts/UI/UISceneLoader.cs
@@ -0,0 +1,46 @@
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UISceneLoader : StaticMonoBehaviour<UISceneLoader>
+{
+    private static readonly string _uiSceneName = "UIScene";
+
+    [SerializeField]
+    private string[] _blacklistedScenes;
+
+    public void SceneChangedHandler(Scene currentScene, Scene nextScene)
+    {
+        Debug.Log($"Moving from {currentScene} to {nextScene}");
+        var uiScene = SceneManager.GetSceneByName(_uiSceneName);
+
+        if (_blacklistedScenes.Contains(nextScene.name))
+        {
+            Debug.Log("UI scene blacklisted on current scene. Will not load.");
+
+            if (uiScene.isLoaded)
+                SceneManager.UnloadSceneAsync(uiScene.name);
+
+            return;
+        }
+
+        if (!uiScene.isLoaded)
+        {
+            Debug.Log("Loading UI scene additively...");
+            SceneManager.LoadSceneAsync(_uiSceneName, LoadSceneMode.Additive);
+        }
+        else
+        {
+            Debug.Log("UI scene is already loaded.");
+        }
+    }
+    private void OnEnable()
+    {
+        SceneManager.activeSceneChanged += SceneChangedHandler;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.activeSceneChanged -= SceneChangedHandler;
+    }
+}
